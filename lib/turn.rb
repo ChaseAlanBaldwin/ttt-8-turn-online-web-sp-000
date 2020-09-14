b027c487ae88c0f415df7762b4fef08e77f70608
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-09-14 00:51:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,30 @@ end
 def move(board, index, character = "X")
   board[index] = character
 end
+
+def valid_move?(board, index)
+  if index > board.length
+    return nil
+  end
+  if position_taken?(board, index) == true
+    return false
+  end
+  if position_taken?(board, index) == false
+  return true
+end
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  end
+  if board[index] == ""
+    return false
+  end
+  if board[index] == nil
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
